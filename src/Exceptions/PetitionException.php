31a806081a6fee5f30d9e71dedc8bbc789be7542
daycry <daycry9@gmail.com>
2021-03-09 17:22:53 +0100

    Delete PetitionException.php

--- src/Exceptions/PetitionException.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Api\Exceptions;
-
-class PetitionException extends \RuntimeException implements PetitionInterface
-{
-    public static function centerSimilar( array $centers = [] )
-    {
-        return new self( lang( 'Center.centerSimilar', array( 'centers' => implode( ',', $centers ) ) ) );
-    }
-}
