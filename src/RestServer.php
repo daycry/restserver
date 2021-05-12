30d64fc3efb77ee53f5a3d64aba4198583587b36
daycry <daycry9@gmail.com>
2021-05-12 14:49:40 +0200

    Check Range IP
    Check Range IP

--- src/RestServer.php
+++ src/RestServer.php
@@ -624,18 +624,26 @@ class RestServer extends ResourceController
                 // Check for a list of valid ip addresses
                 if( isset( $row->ip_addresses ) )
                 {
-                    // multiple ip addresses must be separated using a comma, explode and loop
-                    $list_ip_addresses = explode( ',', $row->ip_addresses );
                     $ip_address = $this->request->getIPAddress();
                     $found_address = false;
 
-                    foreach( $list_ip_addresses as $list_ip )
+                    if( strpos( $row->ip_addresses, '/' ) !== false )
                     {
-                        if ( $ip_address === trim( $list_ip ) )
+                        //check IP is in the range
+                        $found_address = $this->_ipInRange( $ip_address, $row->ip_addresses );
+
+                    }else{
+                        // multiple ip addresses must be separated using a comma, explode and loop
+                        $list_ip_addresses = explode( ',', $row->ip_addresses );
+                        
+                        foreach( $list_ip_addresses as $list_ip )
                         {
-                            // there is a match, set the the value to TRUE and break out of the loop
-                            $found_address = true;
-                            break;
+                            if ( $ip_address === trim( $list_ip ) )
+                            {
+                                // there is a match, set the the value to TRUE and break out of the loop
+                                $found_address = true;
+                                break;
+                            }
                         }
                     }
 
@@ -724,5 +732,28 @@ class RestServer extends ResourceController
         }
 
         return true;
+    }
+
+    /**
+     * Check if a given ip is in a network
+     * @param  string $ip    IP to check in IPV4 format eg. 127.0.0.1
+     * @param  string $range IP/CIDR netmask eg. 127.0.0.0/24, also 127.0.0.1 is accepted and /32 assumed
+     * @return boolean true if the ip is in this range / false if not.
+     */
+    private function _ipInRange( $ip, $range )
+    {
+        if( strpos( $range, '/' ) == false )
+        {
+            $range .= '/32';
+        }
+
+        // $range is in IP/CIDR format eg 127.0.0.1/24
+        list( $range, $netmask ) = explode( '/', $range, 2 );
+        $range_decimal = ip2long( $range );
+        $ip_decimal = ip2long( $ip );
+        $wildcard_decimal = pow( 2, ( 32 - $netmask ) ) - 1;
+        $netmask_decimal = ~ $wildcard_decimal;
+
+        return ( ( $ip_decimal & $netmask_decimal ) == ( $range_decimal & $netmask_decimal ) );
     }
 }
